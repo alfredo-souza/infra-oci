5f96fc51f1d6d4fef18f7e27e88ca13e84692814
a111134 <alfredo.souza@sulamerica.com.br>
2026-07-23 15:27:37 -0300

    importar uma vcn de teste

---
+++ imports.tf
@@ -0,0 +1,8 @@
+import {
+  to = oci_core_vcn.minha_rede_manual
+  id = "ocid1.vcn.oc1.sa-vinhedo-1.amaaaaaav23halaaxrc3qtvpgcultyci6ia4wngeiapsha362g4kiutx72vq" # Pegue esse OCID lá no painel da OCI
+}
+
+resource "oci_core_vcn" "minha_rede_manual" {
+  # Você pode deixar vazio no início, o Terraform Cloud vai te ajudar
+}
