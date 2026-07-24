f4b6066af9077923a34e042f8edf544a7f46f282
a111134 <alfredo.souza@sulamerica.com.br>
2026-07-24 16:04:13 -0300

    removendo drs do import

--- foundation/imports.tf
+++ foundation/imports.tf
@@ -8,14 +8,6 @@ import {
   id = "ocid1.compartment.oc1..aaaaaaaah4jvhdxfo5wkj2crsplif6ebgkmovn6cenacp5zdiutlp3xx2jlq"
 }
 import { 
-  to = oci_identity_compartment.dr_foundation_infra 
-  id = "ocid1.compartment.oc1..aaaaaaaaqlfp22so33giqhkv6sjcmv75lpfppmf5utoqtrluo6q3tkmgsvaa"
-}
-import { 
-  to = oci_identity_compartment.dr_workload 
-  id = "ocid1.compartment.oc1..aaaaaaaaf45ies5dykfwi75mxnqrnen2bkva3b6zglhbaswjbtlpcypxpeca"
-}
-import { 
   to = oci_identity_compartment.sas_database 
   id = "ocid1.compartment.oc1..aaaaaaaatr73dg7ssxzurq7o56f53hfclj55rzgvtowx6q5atpztqfne3bbq"
 }
@@ -42,26 +34,6 @@ import {
 import { 
   to = oci_identity_compartment.sas_exadata_stg 
   id = "ocid1.compartment.oc1..aaaaaaaa4cudj3zgkywjvsyre2uik2kb4ql37d3nhodkfp3wly3axhla37eq"
-}
-
-# --- FILHOS DE dr_foundation_infra ---
-import { 
-  to = oci_identity_compartment.dr_network 
-  id = "ocid1.compartment.oc1..aaaaaaaavizr5a74bwk3znwoi4yhmpozwky3nbqqgyuq3m4ws5wmogjeobla"
-}
-import { 
-  to = oci_identity_compartment.dr_security 
-  id = "ocid1.compartment.oc1..aaaaaaaamzqaxgclryqcpqazxzt4h5xdbm7ip2m47d3goy6ywjrhkvbftuxq"
-}
-import { 
-  to = oci_identity_compartment.dr_shared 
-  id = "ocid1.compartment.oc1..aaaaaaaanybeii6ghaebfsjgethmtzjju2okjlfse2fzkhsjwkwicuri55jq"
-}
-
-# --- FILHOS DE dr_workload ---
-import { 
-  to = oci_identity_compartment.dr_sas_exadata_prd 
-  id = "ocid1.compartment.oc1..aaaaaaaaytjz4uyr6jsahlfeggqh43uw5dr6cw76oacb3r5sigmvgv4icvpa"
 }
 
 # --- IMPORTAÇÃO DAS VCNs ---
