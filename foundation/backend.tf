2d028f2dda14003cdb3dc891e2818668300b6508
a111134 <alfredo.souza@sulamerica.com.br>
2026-07-31 08:03:20 -0300

    remove comentario

--- foundation/backend.tf
+++ foundation/backend.tf
@@ -2,7 +2,7 @@ terraform {
   cloud {
     organization = "Docway"
     workspaces {
-      name = "infra-oci-foundation" # troque para -qa-, -stage-, -prod- conforme o ambiente
+      name = "infra-oci-foundation" 
     }
   }
 }
