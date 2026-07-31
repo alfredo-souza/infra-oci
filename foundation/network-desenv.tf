8b093a523e4208e69e6ef02e3bb3090718953131
a111134 <alfredo.souza@sulamerica.com.br>
2026-07-31 10:23:17 -0300

    import desenv

--- foundation/network-desenv.tf
+++ foundation/network-desenv.tf
@@ -14,11 +14,18 @@ resource "oci_core_subnet" "desenv_pub" {
   compartment_id             = oci_identity_compartment.network.id
   vcn_id                     = oci_core_vcn.desenv.id
   cidr_block                 = "10.67.16.0/20"
-  display_name               = "sub-sa-vinhedo-1-desenv-pub"
+  display_name               = "sub-sa-saopaulo-1-desenv-pub" # Ajustado para o nome real na OCI
   dns_label                  = "subdesenvpub"
   prohibit_public_ip_on_vnic = false
-  route_table_id             = oci_core_route_table.desenv_pub.id
-  security_list_ids          = [oci_core_security_list.desenv_pub.id]
+  
+  # Na OCI, esta subnet está apontando para a Route Table Default
+  route_table_id             = oci_core_route_table.desenv_default.id
+  
+  # Mantendo as duas Security Lists que estão ativas na OCI
+  security_list_ids = [
+    oci_core_security_list.desenv_pub.id,
+    oci_core_security_list.desenv_default.id
+  ]
 }
 
 resource "oci_core_subnet" "desenv_priv" {
@@ -28,8 +35,15 @@ resource "oci_core_subnet" "desenv_priv" {
   display_name               = "sub-sa-vinhedo-1-desenv-priv"
   dns_label                  = "subdesenvpriv"
   prohibit_public_ip_on_vnic = true
-  route_table_id             = oci_core_route_table.desenv_priv.id
-  security_list_ids          = [oci_core_security_list.desenv_priv.id]
+  
+  # Na OCI, esta subnet está apontando para a Route Table Default
+  route_table_id             = oci_core_route_table.desenv_default.id
+  
+  # Mantendo as duas Security Lists que estão ativas na OCI
+  security_list_ids = [
+    oci_core_security_list.desenv_priv.id,
+    oci_core_security_list.desenv_default.id
+  ]
 }
 
 # --- ROUTE TABLES ---
